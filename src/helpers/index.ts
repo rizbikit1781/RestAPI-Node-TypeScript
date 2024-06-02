b68dc5d958e360112c6175e85e4bfe00aa0cac6d
rizbikit1781 <112736862+rizbikit1781@users.noreply.github.com>
2024-06-01 23:21:59 -0600

    Initial commit

---
+++ src/helpers/index.ts
@@ -0,0 +1,13 @@
+//authentication helpers => encrypt the password or randomly the token
+
+import crypto from "crypto";
+
+const SECRET = "PARADON-REST-API";
+
+export const random = () => crypto.randomBytes(128).toString("base64");
+export const authentication = (salt: string, password: string) => {
+  return crypto
+    .createHmac("sha256", [salt, password].join("/"))
+    .update(SECRET)
+    .digest("hex");
+};
